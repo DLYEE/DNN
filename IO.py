935dff743e26eda9effab5c60574494e09185726
floweronhead <b02901030@ntu.edu.tw>
2015-11-27 14:41:04 +0800

    create hmm

--- IO.py
+++ IO.py
@@ -1,10 +1,11 @@
 import numpy as np
 import re
+import cPickle as pickle
+import theano
 
+theano.config.floatX = 'float64'
 
-# input data is a 2-dim list
 def readFile(f):
-
     inputData = {}
     keyOrder = []
     my_file = open(f,"r+")
@@ -16,12 +17,94 @@ def readFile(f):
         inputData[s[0]] = np.asarray(s[1:])
         keyOrder.append(s[0])
     my_file.close()
+    print "type of readFile inputData = ", type(imputData[keyOrder[0]][0])
+    return inputData, keyOrder
+
+def transProb(label):
+    startProb = []
+    endProb = []
+    succesiveProb = []
+    #y0 = aa, y1 = aa...followed the order in str2int
+    #startProb = [P(y0|start), P(y1|start)...]
+    #endProb = [P(y0|end), P(y1|end)...]
+    #succesiveProb = [[P(y0|y0), P(y1|y0)...],[P(y0|y1),P(y1,y1)...]...]
+    sequenceName = ''
+    currentLabel = ''
+    lastLabel = ''
+    startCount = np.zeros(49) #startCount[48] is for total number
+    endCount = np.zeros(49)
+    succesiveCount = np.zeros((48,49))
+    labelFile = open(label, 'r+')
+    while True:
+        line = labelFile.readline()
+        if not line:
+            endCount[48] += 1
+            endCount[str2int(lastLabel)] += 1
+            break        
+        s = re.split(',|_|\n',line)  
+        #s[0] = maeb0, s[1] = sil411, s[2] = 1, s[3] = sil
+        currentLabel = s[3]
+        if sequenceName != (s[0] + s[1]):
+            if sequenceName != '':
+                endCount[48] += 1
+                endCount[str2int(lastLabel)] += 1
+            sequenceName = s[0] + s[1]
+            startCount[48] += 1
+            startCount[str2int(currentLabel)] += 1
+        else:
+            succesiveCount[str2int(lastLabel)][str2int(currentLabel)] += 1
+            succesiveCount[str2int(lastLabel)][48] += 1
+        lastLabel = s[3]
+    for i in range(0,48):
+        subProb = []
+        startProb.append(float(startCount[i]/max(startCount[48],1)))
+        endProb.append(float(endCount[i]/max(endCount[48],1)))
+        for j in range(0,48):
+            subProb.append(float(succesiveCount[i][j]/max(succesiveCount[i][48],1)))
+        succesiveProb.append(subProb)
+
+def readPickle(label, possibility):
+    keyOrder = []
+    inputData = {}
+    with open(possibility, 'rb') as f:
+        y_prob= pickle.load(f)
+        y = pickle.load(f)
+        y_idx = pickle.load(f)
+    labelFile = open(label,'r+')
+    for line in open(label):
+        line = labelFile.readline()
+        s = re.split(',| |\n',line)
+        keyOrder.append(s[0])
+    labelFile.close()
+    count = 0
+    for key in keyOrder:
+        inputData[key] = np.asarray([float(x) for x in y_prob[count]])
+        count += 1
+    return inputData, keyOrder
+
+def dnnReadFile(f1, f2):
+    inputData = {}
+    keyOrder = []
+    my_file = open(f1,"r+")
+    for line in open(f1):
+        line = my_file.readline()
+        s = re.split(" |\n",line)
+        s.pop()
+        s[1:] = [float(x) for x in s[1:]]
+        inputData[s[0]] = s[1:]
+        keyOrder.append(s[0])
+    my_file.close()
+    my_file = open(f2,"r+")
+    for line in open(f2):
+        line = my_file.readline()
+        s = re.split(" |\n",line)
+        s.pop()
+        s[1:] = [float(x) for x in s[1:]]
+        inputData[s[0]] = np.asarray(inputData[s[0]] + s[1:])
     return inputData, keyOrder
 
 
-# input data is a 2-dim list
 def readLabel(f, featureSize):
-
     label = {}
     my_file = open(f,'r+')
     for line in open(f):
@@ -40,28 +123,166 @@ def readLabel(f, featureSize):
     return label
 
 
-def writeFile(f1, f2, possibilityVectors, outputData, keyOrder):
+def writeFile(f1, f2, possibilityVectors, outputData, keyOrder, nnType):
 
+    print "Writing file..."
     file = open(f1,"w")
     file.write('Id,Prediction' + '\n')
     for index in range(len(keyOrder)):
         if index != 0 and index != len(keyOrder) - 1  :
-            # print index, keyOrder[index]
             if outputData[keyOrder[index-1]] == outputData[keyOrder[index+1]] :
                 outputData[keyOrder[index]] = outputData[keyOrder[index-1]]
+            elif outputData[keyOrder[index-1]] != outputData[keyOrder[index]] and outputData[keyOrder[index]] != outputData[keyOrder[index+1]]:
+                outputData[keyOrder[index]] = outputData[keyOrder[index-1]]
+            elif outputData[keyOrder[index]] == outputData[keyOrder[index+1]] and outputData[keyOrder[index]] != outputData[keyOrder[index+2]]:
+                outputData[keyOrder[index]] = outputData[keyOrder[index-1]]
+                outputData[keyOrder[index+1]] = outputData[keyOrder[index-1]]
         outputData[keyOrder[index]] = mrg48to39(outputData[keyOrder[index]])
         file.write(keyOrder[index] + ',' + outputData[keyOrder[index]] + '\n')
     file.close()
-    np.savetxt(f2, possibilityVectors, delimiter=",")
-    # file = open(f2,"w")
-    # for index in range(len(keyOrder)):
-        # file.write(keyOrder[index] + ',')
-        # # file.write(possibilityVector[keyOrder[index]].tobytes())
-        # np.savetxt(f2, possibilityVector[keyOrder[index]])
-        # # possibilityVector[keyOrder[index]].tofile(f2, "text")
-        # file.write('\n')
-    # file.close()
+    if nnType == 'dnn':
+        file = open(f2,"w")
+        for line in possibilityVectors:
+            for index in range(len(line)):
+                if index != len(line) - 1:
+                    file.write(str(line[index]) + " ")
+                else:
+                    file.write(str(line[index]))
+            file.write('\n')
+        file.close()
 
+
+def trimOutput(f1, f2):
+
+    my_file = open(f1,"r+")
+    frames = []
+    for line in open(f1):
+        line = my_file.readline()
+        if line[:2] == 'Id':
+            continue
+        else:
+            phone = re.split("_|,|\n",line)
+            phone.pop()
+            frames.append(phone)
+    my_file.close()
+    file = open(f2,"w")
+    file.write('id,phone_sequence' + '\n')
+    file.write(frames[0][0] + "_" + frames[0][1] + ",")
+    file.write(idx2chr(frames[0][3]))
+    for index in range(len(frames)):
+        if index == 0:
+            continue
+        if frames[index][:2] == frames[index-1][:2]:
+            if frames[index][3] != frames[index-1][3]:
+                file.write(idx2chr(frames[index][3]))
+        else:
+            file.write('\n' + frames[index][0] + "_" + frames[index][1] + "," + idx2chr(frames[index][3]))
+            #file.write("\n")
+    file.close()
+
+
+def idx2chr(string):
+    value = -1
+    if string == "aa":
+        value = 'a'
+    elif string == "ae":
+        value = 'b'
+    elif string == "ah":
+        value = 'c'
+    elif string == "aw":
+        value = 'e'
+    elif string == "ay":
+        value = 'g'
+    elif string == "b":
+        value = 'h'
+    elif string == "ch":
+        value = 'i'
+    elif string == "sil":
+        value = 'L'
+    elif string == "d":
+        value = 'k'
+    elif string == "dh":
+        value = 'l'
+    elif string == "dx":
+        value = 'm'
+    elif string == "eh":
+        value = 'n'
+    elif string == "l":
+        value = 'B'
+    elif string == "n":
+        value = 'D'
+    elif string == "er":
+        value = 'r'
+    elif string == "ey":
+        value = 's'
+    elif string == "f":
+        value = 't'
+    elif string == "g":
+        value = 'u'
+    elif string == "hh":
+        value = 'v'
+    elif string == "ih":
+        value = 'w'
+    elif string == "iy":
+        value = 'y'
+    elif string == "jh":
+        value = 'z'
+    elif string == "k":
+        value = 'A'
+    elif string == "m":
+        value = 'C'
+    elif string == "ng":
+        value = 'E'
+    elif string == "ow":
+        value = 'F'
+    elif string == "oy":
+        value = 'G'
+    elif string == "p":
+        value = 'H'
+    elif string == "r":
+        value = 'I'
+    elif string == "sh":
+        value = 'K'
+    elif string == "s":
+        value = 'J'
+    elif string == "th":
+        value = 'N'
+    elif string == "t":
+        value = 'M'
+    elif string == "uh":
+        value = 'O'
+    elif string == "uw":
+        value = 'P'
+    elif string == "v":
+        value = 'Q'
+    elif string == "w":
+        value = 'S'
+    elif string == "y":
+        value = 'T'
+    elif string == "z":
+        value = 'U'
+    elif string == "ao":
+        value = 'd'
+    elif string == "ax":
+        value = 'f'
+    elif string == "epi":
+        value = 'q'
+    elif string == "cl":
+        value = 'j'
+    elif string == "vcl":
+        value = 'R'
+    elif string == "el":
+        value = 'o'
+    elif string == "en":
+        value = 'p'
+    elif string == "ix":
+        value = 'x'
+    elif string == "zh":
+        value = 'V'
+    if value != -1:
+        return value
+    else:
+        print ("input string is not fit!\n")
 
 
 def mrg48to39(string):
@@ -181,7 +402,7 @@ def str2int(string):
     if value != -1:
         return value
     else:
-        print "input string is not fit!\n"
+        print ("input string is not fit!\n")
 
 
 def int2str(num):
@@ -285,4 +506,4 @@ def int2str(num):
     if string != "":
         return string
     else:
-        print "input number is not available!\n"
+        print ("input number is not available!\n")
