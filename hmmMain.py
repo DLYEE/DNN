5bb4f73282492160a30f5c9b3abd8d40a295278c
floweronhead <b02901030@ntu.edu.tw>
2015-11-30 00:12:01 +0800

    implement viterbi

--- hmmMain.py
+++ hmmMain.py
@@ -3,6 +3,7 @@ import re
 import math
 import hmmFunc
 import numpy as np
+import time
 
 def viterbi():
     observationProb, keyOrder = IO.readFile('test.prb')
@@ -12,26 +13,33 @@ def viterbi():
     value = []
     trace = []
     def forward(key, preValue, preTrace):
+        reValue = []
+        reTrace = []
         for i in range(0,48):
-            tempValue = [(math.log(preValue[prei]) +
+            tempValue = [preValue[prei] +
                          math.log(transMatrix[prei][i]) +
-                         math.log(observationProb[key][i]), 
-                         preTrace[prei].append(IO.int2str(i))) for prei in range(0,48)]
-            maxSequence = reduce(lambda x1,x2: x2 if x2[0] > x1[0] else x1, tempValue)  
-            value[i] = maxSequence[0]
-            trace[i] = maxSequence[1]
-        return value, trace
-
+                         math.log(observationProb[key][i]) for prei in range(0,48)]
+            maxIndex = np.argmax(tempValue)
+            reValue.append(tempValue[maxIndex])
+            tempTrace = preTrace[maxIndex][:]
+            reTrace.append(tempTrace)
+            reTrace[i].append(IO.int2str(i))
+        return reValue, reTrace
+    
+    timeStart = time.time()
     for index in range(len(keyOrder)):
+        if index % 1000 == 0:
+            timeEnd = time.time()
+            print "Time Cost = ", timeEnd - timeStart
+            timeStart = time.time()
         if index == 0:
             for i in range(0,48):
-                value.append(observationProb[keyOrder[0]][i])
+                value.append(math.log(observationProb[keyOrder[index]][i]))
                 trace.append([IO.int2str(i)])
         else:
             value, trace = forward(keyOrder[index], value, trace)
-    maxIndex = argmax(value)
+    maxIndex = np.argmax(value)
     print 'max value = ', value[maxIndex]
-    print 'trace = ', trace[maxIndex]
     #preValue = [1.3212,....]
     #preTrace = [sil, d, s, ...]
 
